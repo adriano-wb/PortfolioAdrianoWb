f0ff2a8ec4529dc63f00c34733db8712f980e372
adriano-wb <126106701+adriano-wb@users.noreply.github.com>
2023-08-17 14:36:37 -0300

    Add files via upload

--- js/menu_marcador.js
+++ js/menu_marcador.js
@@ -1,20 +1,9 @@
-// Obter a altura de todas as seções da página
-const secao01Altura = $("#apresentacao").outerHeight();
-const secao02Altura = $("#sobre").outerHeight();
-const secao03Altura = $("#portfolio").outerHeight();
-const parallax01Altura = $("#para01").outerHeight();
-const parallax02Altura = $("#para02").outerHeight();
-const parallax03Altura = $("#para03").outerHeight();
-
 // Função para remover classe .w3-white do menu de navegação
 const removeClasseTeal = () => $(".w3-links-js").removeClass("w3-white");
 
 export default function menuMarcador() {
    // Remover classe .w3-white de todos os links do menu
    removeClasseTeal();
-
-   // Adicionar a classe .w3-white ao link clicado
-   // $(this).addClass("w3-white");
 
    // Se a página estiver na seção "Home"
    if ($(window).scrollTop() >= 0) {
@@ -27,8 +16,7 @@ export default function menuMarcador() {
    }
 
    // Se a página estiver na seção "Sobre"
-   // e um pouco menos de 50px de altura
-   if ($(window).scrollTop() >= (secao01Altura + parallax01Altura - 150)) {
+   if ($(window).scrollTop() >= $("#sobre").offset().top) {
       // Remover classe .w3-white de todos os links do menu
       removeClasseTeal();
 
@@ -37,13 +25,8 @@ export default function menuMarcador() {
       $(".w3-menu .w3-links-js").eq(0).addClass("w3-white");
    }
 
-   // Se a página estiver na seção "Portfólio"
-   // e um pouco menos de 50px de altura
-   if ($(window).scrollTop() >= (
-      secao01Altura +
-      parallax01Altura +
-      secao02Altura +
-      parallax02Altura - 150)) {
+   // Se a página estiver na seção "Portfélio"
+   if ($(window).scrollTop() >= $("#portfolio").offset().top) {
       // Remover classe .w3-white de todos os links do menu
       removeClasseTeal();
 
@@ -53,14 +36,7 @@ export default function menuMarcador() {
    }
 
    // Se a página estiver na seção "Contato"
-   // e um pouco menos de 50px de altura
-   if ($(window).scrollTop() >= (
-      secao01Altura +
-      parallax01Altura +
-      secao02Altura +
-      parallax02Altura +
-      secao03Altura +
-      parallax03Altura - 50)) {
+   if ($(window).scrollTop() >= $("#contato").offset().top) {
       // Remover classe .w3-white de todos os links do menu
       removeClasseTeal();
 
